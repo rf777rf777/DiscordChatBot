2da17d762190a63d4188a344307bc07b7f2809d9
rf777rf777 <rf777rf777@gmail.com>
2025-04-21 11:19:48 +0800

    merge

--- src/api/v1/sd_routes.py
+++ src/api/v1/sd_routes.py
@@ -5,7 +5,7 @@ from app.sd_service import create_sd_image, image_to_image
 from core.config import settings
 import asyncio
 
-# router初始化
+# router初始化 #4234457591
 router = APIRouter(prefix=f'/{settings.ROUTER_NAME_SDImage}', 
                    tags=[settings.ROUTER_NAME_SDImage])
 @router.post('', summary = 'Stable Diffusion 圖片生成塔羅牌', 
@@ -22,41 +22,6 @@ async def use_stable_diffusion(
     
     # 使用 create_sd_image 函數生成圖片
     img_byte_arr = await create_sd_image(
-        mode_name = model_name,
-        vae_name = vae_name,
-        prompt = prompt,
-        sampler_name = sampler,
-        seed = seed,
-        steps = steps
-    )    
-
-    
-    # 使用 StreamingResponse 返回處理後的圖片，並包含 filename
-    # headers = {
-    #     'Content-Disposition': f'attachment; filename="test.png"'
-    # }
-    
-    return StreamingResponse(img_byte_arr, 
-                             media_type = "image/png"
-                             #, headers = headers
-                             )
-    
-@router.put('', summary = 'Img to img', 
-            description = 'Img to img',
-            response_class = StreamingResponse,
-            responses = {200: {"content": {"image/png": {}}}})
-async def img_to_img(
-    file: UploadFile = File(...),
-    model_name: str = Query(description='模型名稱', default = 'qchanAnimeMix_v40'),
-    vae_name: str = Query(description='VAE名稱', default = ''),
-    prompt: str = Query(description='提示詞', default = 'masterpiece, best quality, 1girl'),
-    sampler: str = Query(description='取樣器名稱', default = 'euler_a'),
-    seed: int = Query(description='隨機種子', default = None),
-    steps: int = Query(description='取樣步驟', default = 40)): 
-    
-    #
-    img_byte_arr = await image_to_image(
-        file,
         mode_name = model_name,
         vae_name = vae_name,
         prompt = prompt,
--- src/app/sd_service.py
+++ src/app/sd_service.py
@@ -1,4 +1,7 @@
 import os, io, asyncio
+os.environ["PYTORCH_ENABLE_cuda_FALLBACK"] = "1"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+from datetime import datetime
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 from datetime import datetime
 import random
@@ -20,7 +23,7 @@ async def create_sd_image(mode_name,
                     sampler_name="euler_a",
                     seed=None,
                     steps=40,
-                    torch_dtype = torch.float32, 
+                    torch_dtype = torch.float16, 
                     # 禁用 safety checker
                     safety_checker=None):
     await asyncio.sleep(0.2)
@@ -28,11 +31,11 @@ async def create_sd_image(mode_name,
     pipe = StableDiffusionPipeline.from_pretrained(
         f'src/Models/SD/BaseModels/{mode_name}',
         torch_dtype=torch_dtype,
-        safety_checker=safety_checker).to("mps")
+        safety_checker=safety_checker).to("cuda")
     
     if vae_name:
         pipe.vae = AutoencoderKL.from_pretrained(f'src/Models/SD/VAE/{vae_name}', 
-                                                 torch_dtype=torch_dtype).to("mps")
+                                                 torch_dtype=torch_dtype).to("cuda")
 
     sampler_factory = _get_scheduler_factory(sampler_name)
     pipe.scheduler = sampler_factory(pipe.scheduler.config)
@@ -87,18 +90,19 @@ async def create_sd_image(mode_name,
         print(f"Seed: {target_seed}")
         generator = [torch.manual_seed(seed + i) for i in range(num_images)]
         # generator = torch.manual_seed(target_seed)
-        image = pipe(
-            prompt=prompt,
-            negative_prompt= "easynegative,badhandv4,watermark", #"FastNegativeV2, badhandv4, watermark",
-            num_inference_steps=steps,
-            guidance_scale=guidance_scale,
-            generator=generator,
-            height=height,
-            width=width,
-            num_images_per_prompt=num_images
-        ).images[0]
-        imageList.append(image)
-        torch.mps.empty_cache()
+        with torch.inference_mode():
+            image = pipe(
+                prompt=prompt,
+                negative_prompt= "easynegative,badhandv4,watermark", #"FastNegativeV2, badhandv4, watermark",
+                num_inference_steps=steps,
+                guidance_scale=guidance_scale,
+                generator=generator,
+                height=height,
+                width=width,
+                num_images_per_prompt=num_images
+            ).images[0]
+            imageList.append(image)
+        torch.cuda.empty_cache()
         gc.collect()
         
     # 儲存影像至記憶體(bytes)
@@ -114,7 +118,7 @@ async def image_to_image(file, mode_name,
                     sampler_name="euler_a",
                     seed=None,
                     steps=40,
-                    torch_dtype = torch.float32, 
+                    torch_dtype = torch.float16, 
                     # 禁用 safety checker
                     safety_checker=None):
     contents = await file.read()
@@ -140,9 +144,9 @@ async def image_to_image(file, mode_name,
     
     if vae_name:
         pipe.vae = AutoencoderKL.from_pretrained(f'src/Models/SD/VAE/{vae_name}', 
-                                                 torch_dtype=torch_dtype).to("mps")
+                                                 torch_dtype=torch_dtype).to("cuda")
 
-    pipe = pipe.to("mps")  # 或 "mps" 根據你的設備
+    pipe = pipe.to("cuda")  # 或 "cuda" 根據你的設備
     pipe.enable_attention_slicing()
     pipe.load_textual_inversion("src/Models/SD/Embeddings/easynegative.safetensors", token="easynegative", mean_resizing=False)
     pipe.load_textual_inversion("src/Models/SD/Embeddings/badhandv4.pt", token="badhandv4", mean_resizing=False)
@@ -170,7 +174,8 @@ async def image_to_image(file, mode_name,
         generator=generator
     ).images[0]
     
-    torch.mps.empty_cache()
+    #torch.mps.empty_cache()
+    torch.cuda.empty_cache()
     gc.collect()
         
     # 儲存影像至記憶體(bytes)
@@ -195,6 +200,49 @@ def _get_scheduler_factory(sampler_name: str):
     extra_kwargs = sampler_info.get("kwargs", {})
 
     return lambda config: SchedulerCls.from_config(config, **extra_kwargs)
+
+def get_canny_img(input_image):
+    # --- 利用 OpenCV 生成 Canny 邊緣圖 ---
+    # 轉換成 numpy 陣列，並轉成灰階
+    np_image = np.array(input_image)
+    gray = cv2.cvtColor(np_image, cv2.COLOR_RGB2GRAY)
+    # 使用 Canny 偵測邊緣，這裡參數可依需求調整
+    edges = cv2.Canny(gray, threshold1=120, threshold2=250)
+    # 將邊緣圖轉回 RGB PIL Image
+    control_image = Image.fromarray(cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB))
+
+    nowtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    control_image.save(f"temp_{nowtime}.jpg")
+    return control_image
+
+def get_resized_image(img, bigger:bool=False, smaller:bool=False) -> Image.Image:
+    img = img
+    w, h = img.size
+    if bigger:
+        resized_img = img.resize((w*2,h*2), Image.BICUBIC)
+        return resized_img
+    if smaller:
+        resized_img = img.resize((w//2,h//2), Image.LANCZOS)
+        return resized_img
+    # --- 計算新的大小 ---
+    if w == h:
+        new_size = (512, 512)
+    elif w > h:
+        new_w = 768
+        new_h = int(h * (768 / w))
+        new_size = (new_w, new_h)
+    else:
+        new_h = 768
+        new_w = int(w * (768 / h))
+        if new_w > 512:
+            new_w = 512
+        new_size = (new_w, new_h)
+    
+    # BICUBIC適合放大圖像
+    # LANCZOS適合縮小圖像
+    resized_img = img.resize(new_size, Image.LANCZOS)
+
+    return resized_img
 
 def get_canny_img(input_image):
     # --- 利用 OpenCV 生成 Canny 邊緣圖 ---
